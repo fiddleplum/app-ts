ceff18e1ce9c83371ad2294b8b0abcde3d3d333e
Stephen Hurley <stephenhurley@gmail.com>
2020-04-24 15:36:35 -0700

    Small fixes.

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ import SimpleApp from './simple_app';
 import Component from './component';
 // import Font from './font';
 import Icon from './icon';
+import isIn from './is_in';
 import Router from './router';
 import ShowHide from './show_hide';
 // import State from './state';
@@ -15,6 +16,7 @@ export {
 	Component,
 	// Font,
 	Icon,
+	isIn,
 	Router,
 	ShowHide,
 	// State,
