fef39ed012456e02567bb71367c9b0a008925689
Stephen Hurley <stephenhurley@gmail.com>
2020-10-20 05:02:30 -0700

    Clean up exports.

--- src/index.ts
+++ src/index.ts
@@ -1,25 +1,23 @@
 import { App } from './app';
-import { SimpleApp } from './simple_app';
 import { Component } from './component';
-// import Font from './font';
 import { Icon } from './icon';
 import { isIn } from './is_in';
+import { JSONType } from './json_type';
 import { Router } from './router';
 import { ShowHide } from './show_hide';
-// import State from './state';
+import { SimpleApp } from './simple_app';
 import { UniqueIds } from './unique_ids';
 import { WS } from './ws';
 
 export {
 	App,
-	SimpleApp,
 	Component,
-	// Font,
 	Icon,
 	isIn,
+	JSONType,
 	Router,
 	ShowHide,
-	// State,
+	SimpleApp,
 	UniqueIds,
 	WS
 };
