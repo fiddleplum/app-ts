962d94d47c95f91dd3acfaa6ddc52485ee12243e
Stephen Hurley <stephenhurley@gmail.com>
2020-10-23 18:37:27 -0700

    Added cookies class.

---
+++ src/cookies.ts
@@ -0,0 +1,20 @@
+export class Cookies {
+	/** Gets a cookie.
+	 * @param name - The name of the cookie. */
+	get(name: string): string | undefined {
+		const cookieEntries = document.cookie.split(';');
+		const cookieEntry = cookieEntries.find(cookie => cookie.startsWith(name + '='));
+		if (cookieEntry !== undefined) {
+			return decodeURIComponent(cookieEntry.split('=')[1]);
+		}
+		return undefined;
+	}
+
+	/** Sets a cookie.
+	 * @param name - The name of the cookie.
+	 * @param content - The content of the cookie.
+	 * @param maxAge - The lifetime in seconds of the cookie. */
+	set(name: string, content: string, maxAge: number): void {
+		document.cookie = `${name}=${encodeURIComponent(content)};max-age=${maxAge};path=/;secure`;
+	}
+}
