7ee62303759d5fd3f11bc2df907fedb75c12fc51
Stephen Hurley <stephenhurley@gmail.com>
2021-02-07 14:15:52 -0800

    Made WS connect via function instead of constructor.

--- src/ws.ts
+++ src/ws.ts
@@ -3,118 +3,121 @@ import { JSONType } from './json_type';
 
 /** A message sending and receiving WebSocket interface. */
 export class WS {
-	constructor(url: string) {
-		// Create the web socket.
-		this.webSocket = new WebSocket('wss://' + url);
-
+	connect(url: string): Promise<void> {
 		// Setup the promise that resolves when the web socket is ready to be used.
-		this.readyPromise = new Promise((resolve: () => void, reject: () => void) => {
-			this.promiseResolve = resolve;
-			this.promiseReject = reject;
-		});
+		return new Promise((resolve: () => void, reject: (error: Error) => void) => {
+			// Create the web socket.
+			this.webSocket = new WebSocket('wss://' + url);
 
-		// When the websocket connects...
-		this.webSocket.onopen = (): void => {
-			console.log('Websocket connected.');
-			this.promiseResolve();
-		};
-		// When a message is received...
-		this.webSocket.onmessage = (message: MessageEvent): void => {
-			// Output the message received.
-			console.log('ws.received ' + message.data);
+			// When the websocket connects...
+			this.webSocket.onopen = (): void => {
+				console.log('Websocket connected.');
+				resolve();
+			};
 
-			try {
-				// Get the response.
-				let response: JSONType;
+			// When a message is received...
+			this.webSocket.onmessage = (message: MessageEvent): void => {
+				// Output the message received.
+				console.log('ws.received ' + message.data);
+
 				try {
-					response = JSON.parse(message.data);
+					// Get the response.
+					let response: JSONType;
+					try {
+						response = JSON.parse(message.data);
+					}
+					catch (error) {
+						throw new Error('Response must be valid JSON. ' + error);
+					}
+					if (typeof response !== 'object' || response === null || Array.isArray(response)) {
+						throw new Error('Response must be an object.');
+					}
+
+					// Get the id of the response.
+					const id = response.id;
+					if (typeof id !== 'number') {
+						throw new Error('Response.id must be a number.');
+					}
+
+					// Get the function to be resolved using the response id.
+					const promiseFunctions = this.activeSends.get(id);
+					if (promiseFunctions === undefined) {
+						throw new Error('No active send waiting for response.');
+					}
+
+					// Get the data of the response.
+					const data = response.data;
+					if (typeof data !== 'object' || data === null || Array.isArray(data)) {
+						throw new Error('Response.data must be an object.');
+					}
+
+					// If the response was not a success, call reject the promise function.
+					if (typeof data.success !== 'boolean') {
+						throw new Error('Response.data.success must be a boolean.');
+					}
+					if (data.success === false) {
+						if (typeof data.error !== 'string') {
+							throw new Error('Response.data.error must be a string when response.data.success is false.');
+						}
+						promiseFunctions.reject(new Error(data.error));
+					}
+
+					// Remove the id from the actively sending message list and release the id.
+					this.activeSends.delete(id);
+					this.uniqueIds.release(id);
+
+					// Call the resolve function without the success and error properties.
+					delete data.success;
+					delete data.error;
+					promiseFunctions.resolve(data);
 				}
 				catch (error) {
-					throw new Error('Response must be valid JSON. ' + error);
+					console.log('Error while receiving websocket message.');
+					console.log('  Message: ' + message.data);
+					console.log('  Error: ' + error);
 				}
-				if (typeof response !== 'object' || response === null || Array.isArray(response)) {
-					throw new Error('Response must be an object.');
-				}
+			};
 
-				// Get the id of the response.
-				const id = response.id;
-				if (typeof id !== 'number') {
-					throw new Error('Response.id must be a number.');
-				}
+			// When there is an error in the websocket...
+			this.webSocket.onerror = (event: ErrorEvent): void => {
+				console.log(`Error in websocket.`, event);
+				reject(new Error(`Error in websocket: ${event.message}`));
+			};
 
-				// Get the function to be resolved using the response id.
-				const promiseFunctions = this.activeSends.get(id);
-				if (promiseFunctions === undefined) {
-					throw new Error('No active send waiting for response.');
-				}
+			// When the websocket disconnects...
+			this.webSocket.onclose = (event: CloseEvent): void => {
+				this.webSocket = undefined;
+				console.log(event);
+			};
 
-				// Get the data of the response.
-				const data = response.data;
-				if (typeof data !== 'object' || data === null || Array.isArray(data)) {
-					throw new Error('Response.data must be an object.');
+			// Make sure to close the websocket before unloading the page.
+			window.addEventListener('beforeunload', () => {
+				if (this.webSocket !== undefined) {
+					this.webSocket.close();
+					this.webSocket = undefined;
 				}
-
-				// If the response was not a success, call reject the promise function.
-				if (typeof data.success !== 'boolean') {
-					throw new Error('Response.data.success must be a boolean.');
-				}
-				if (data.success === false) {
-					if (typeof data.error !== 'string') {
-						throw new Error('Response.data.error must be a string when response.data.success is false.');
-					}
-					promiseFunctions.reject(new Error(data.error));
-				}
-
-				// Remove the id from the actively sending message list and release the id.
-				this.activeSends.delete(id);
-				this.uniqueIds.release(id);
-
-				// Call the resolve function without the success and error properties.
-				delete data.success;
-				delete data.error;
-				promiseFunctions.resolve(data);
-			}
-			catch (error) {
-				console.log('Error while receiving websocket message.');
-				console.log('  Message: ' + message.data);
-				console.log('  Error: ' + error);
-			}
-		};
-		// When there is an error in the websocket...
-		this.webSocket.onerror = (event: ErrorEvent): void => {
-			console.log(`Error in websocket.`, event);
-			this.promiseReject();
-		};
-		// When the websocket disconnects...
-		this.webSocket.onclose = (event: CloseEvent): void => {
-			console.log(event);
-		};
-		// Make sure to close the websocket before unloading the page.
-		window.addEventListener('beforeunload', () => {
-			this.webSocket.close();
+			});
 		});
-	}
-
-	/** Gets the promise that resolves when the web socket is ready to be used. */
-	getReadyPromise(): Promise<void> {
-		return this.readyPromise;
 	}
 
 	/** Closes the web socket connection. */
 	close(): void {
-		this.webSocket.close();
+		if (this.webSocket !== undefined) {
+			this.webSocket.close();
+			this.webSocket = undefined;
+		}
 	}
 
 	/** Sends the JSON data along the web socket. Returns a promise resolving with response JSON data. */
 	send(data: JSONType): Promise<{ [prop: string]: (JSONType | undefined) }> {
-		if (this.webSocket.readyState !== WebSocket.OPEN) {
+		if (this.webSocket === undefined || this.webSocket.readyState !== WebSocket.OPEN) {
 			throw new Error('The web socket is not yet connected.');
 		}
 		console.log('ws.send ' + JSON.stringify(data));
 		return new Promise<{ [prop: string]: (JSONType | undefined) }>((resolve, reject) => {
 			const id = this.uniqueIds.get();
 			this.activeSends.set(id, { resolve, reject });
-			this.webSocket.send(JSON.stringify({
+			this.webSocket!.send(JSON.stringify({
 				id: id,
 				data: data
 			}));
@@ -122,16 +125,7 @@ export class WS {
 	}
 
 	/** The WebSocket connection. */
-	private webSocket: WebSocket;
-
-	/** A promise that resolves when the WebSocket connection is open. */
-	private readyPromise: Promise<void>;
-
-	/** The resolve function for the ready promise. */
-	private promiseResolve!: () => void;
-
-	/** The reject function for the ready promise. */
-	private promiseReject!: () => void;
+	private webSocket: WebSocket | undefined;
 
 	/** A collection of unique ids for pairing received messages with sent messages. */
 	private uniqueIds: UniqueIds = new UniqueIds();
